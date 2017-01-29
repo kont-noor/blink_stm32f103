e1b55268763865426a1a36f4db89aaa18f04df8e
Nickolay Kondratenko <devmarkup@gmail.com>
2017-01-29 18:19:18 +0200

    replace pa8 with pc13

--- main.c
+++ main.c
@@ -5,68 +5,68 @@
 #include "stm32f10x.h"
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_tim.h"
- 
+
 /* User defined function prototypes */
-void GPIOA_Init(void);
+void GPIOC_Init(void);
 void TIM2_CC1_Init(void);
 void led_toggle(void);
- 
+
 /* Prototypes for func. from stm32f10x lib. */
 void GPIO_Init(GPIO_TypeDef* GPIOx, GPIO_InitTypeDef* GPIO_InitStruct);
 ITStatus TIM_GetITStatus(TIM_TypeDef* TIMx, uint16_t TIM_IT);
 void TIM_ClearITPendingBit(TIM_TypeDef* TIMx, uint16_t TIM_IT);
- 
+
 int main(void)
 {
-    /* Initialize GPIOA PIN8 */
-    GPIOA_Init();
-     
+    /* Initialize GPIOC PIN13 */
+    GPIOC_Init();
+
     /* Initialize TIM2 Capture/Compare 1 in output compare mode */
     TIM2_CC1_Init();
-     
+
     /* Toggle LED forever */
     while(1)
     {
         /* Do nothing, all happens in ISR */
     }
 }
- 
-/* Initialize GPIOA PIN8 */
-void GPIOA_Init(void)
+
+/* Initialize GPIOC PIN13 */
+void GPIOC_Init(void)
 {
-    /* Configuration info. for PORTA PIN8:
+    /* Configuration info. for PORTC PIN13:
      * - Speed = 50MHz
-     * - Push-pull output mode 
+     * - Push-pull output mode
      */
-    GPIO_InitTypeDef gpioa_pin8_config = { GPIO_Pin_8, 
-                                           GPIO_Speed_50MHz, 
-                                           GPIO_Mode_Out_PP };
-                                             
+    GPIO_InitTypeDef gpioc_pin13_config = { GPIO_Pin_13,
+                                            GPIO_Speed_50MHz,
+                                            GPIO_Mode_Out_PP };
+
     /* Enable PORT A clock */
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;  
-    /* Configure PORTA PIN 8 */
-    GPIO_Init(GPIOA, &gpioa_pin8_config);   
-     
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+    /* Configure PORTC PIN 13 */
+    GPIO_Init(GPIOC, &gpioc_pin13_config);
+
     /* Turn off LED to start with */
-    GPIOA->BSRR = (uint32_t)1 << 8;    
+    GPIOC->BSRR = (uint32_t)1 << 13;
 }
- 
+
 /* Toggle LED */
 void led_toggle(void)
 {
-    /* If PORTA BIT 8 clear, set it */
-    if((GPIOA->ODR & (uint32_t)1<<8) == 0)
+    /* If PORTC BIT 13 clear, set it */
+    if((GPIOC->ODR & (uint32_t)1<<13) == 0)
     {
-        GPIOA->BSRR = (uint32_t)1 << 8;
+        GPIOC->BSRR = (uint32_t)1 << 13;
     }
-    /* If PORTA BIT 8 set, clear it */
+    /* If PORTC BIT 13 set, clear it */
     else
     {
-        GPIOA->BRR = (uint32_t)1 << 8;
+        GPIOC->BRR = (uint32_t)1 << 13;
     }
 }
- 
-/* Configure TIM2 Capture/Compare 1 to work in output compare mode 
+
+/* Configure TIM2 Capture/Compare 1 to work in output compare mode
  * so that the red LED flashes at 1HZ (toggle every 0.5s) */
 void TIM2_CC1_Init(void)
 {
@@ -74,20 +74,20 @@ void TIM2_CC1_Init(void)
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
     /* Enable TIM2 global interrupt */
     NVIC->ISER[0] |= 0x10000000;
-     
+
     /* Frequency after prescalar = 72MHz/(7199+1) = 10KHz.
      * Compare register = 5000 so a compare match occurs every 0.5s.
      */
     TIM2->PSC = (uint16_t)7199;
     TIM2->CCR1 = (uint16_t)5000;
-     
+
     /* Enable Capture/Compare 1 interrupt */
     TIM2->DIER |= (uint16_t)0x0002;
-         
+
     /* Enable TIM2 counter (in upcount mode) */
-    TIM2->CR1 |= (uint16_t)0x0001;   
+    TIM2->CR1 |= (uint16_t)0x0001;
 }
- 
+
 /* Timer 2 Interrupt Service Routine */
 void TIM2_IRQHandler(void)
 {
@@ -100,13 +100,13 @@ void TIM2_IRQHandler(void)
         /* Increment compare register by 5000 so next interrupt
          * occurs in 0.5s */
         TIM2->CCR1 += (uint16_t)5000;
-    }   
-     
+    }
+
     /* ===== Other TIM2 interrupt types can go below ======
      * .........
      */
 }
- 
+
 /* This function comes straight from stm32f10x_gpio.c. All it does is
  * configure a GPIO pin's speed and mode etc. but seems so complicated.
  */
@@ -118,13 +118,13 @@ void GPIO_Init(GPIO_TypeDef* GPIOx, GPIO_InitTypeDef* GPIO_InitStruct)
   /*
   assert_param(IS_GPIO_ALL_PERIPH(GPIOx));
   assert_param(IS_GPIO_MODE(GPIO_InitStruct->GPIO_Mode));
-  assert_param(IS_GPIO_PIN(GPIO_InitStruct->GPIO_Pin));  
+  assert_param(IS_GPIO_PIN(GPIO_InitStruct->GPIO_Pin));
   */
-   
+
 /*---------------------------- GPIO Mode Configuration -----------------------*/
   currentmode = ((uint32_t)GPIO_InitStruct->GPIO_Mode) & ((uint32_t)0x0F);
   if ((((uint32_t)GPIO_InitStruct->GPIO_Mode) & ((uint32_t)0x10)) != 0x00)
-  { 
+  {
     /* Check the parameters */
     /* assert_param(IS_GPIO_SPEED(GPIO_InitStruct->GPIO_Speed)); */
     /* Output mode */
@@ -198,20 +198,20 @@ void GPIO_Init(GPIO_TypeDef* GPIOx, GPIO_InitTypeDef* GPIO_InitStruct)
     GPIOx->CRH = tmpreg;
   }
 }
- 
-/* This function comes straight from stm32f10x_tim.c. It checks 
+
+/* This function comes straight from stm32f10x_tim.c. It checks
  * whether interrupt TIM_IT in TIMx has occurred or not.
  */
 ITStatus TIM_GetITStatus(TIM_TypeDef* TIMx, uint16_t TIM_IT)
 {
-  ITStatus bitstatus = RESET;  
+  ITStatus bitstatus = RESET;
   uint16_t itstatus = 0x0, itenable = 0x0;
   /* Check the parameters */
   //assert_param(IS_TIM_ALL_PERIPH(TIMx));
   //assert_param(IS_TIM_GET_IT(TIM_IT));
-    
+
   itstatus = TIMx->SR & TIM_IT;
-   
+
   itenable = TIMx->DIER & TIM_IT;
   if ((itstatus != (uint16_t)RESET) && (itenable != (uint16_t)RESET))
   {
@@ -223,7 +223,7 @@ ITStatus TIM_GetITStatus(TIM_TypeDef* TIMx, uint16_t TIM_IT)
   }
   return bitstatus;
 }
- 
+
 /* This function comes straight from stm32f10x_tim.c.
  * It clears the TIMx's TIM_IT interrupt pending bits.
  */
